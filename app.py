41048645dd1a7c8c529e2d7747604367b932d55d
@crazyself <crazyself@gmail.com>
2022-07-13 18:31:44 -0300

    acrescentando páginas

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pickle
 from pathlib import Path
-import streamlit as st
 import streamlit_authenticator as stauth
+import streamlit as st
 
 # User authentication
 names = ["Almir Martins", "Leonardo Cordeiro"]
@@ -13,16 +13,27 @@ with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
 authenticator = stauth.Authenticate(
-    names, usernames, hashed_passwords, 
-    'cookie_named_name', 'abcdef', cookie_expiry_days=1)
+    names,
+    usernames,
+    hashed_passwords,
+    "cookie_named_name",
+    "abcdef",
+    cookie_expiry_days=0,
+)
 
-name, authenticattion_status, username = authenticator.login('Login', 'main')
+
+name, authenticattion_status, username = authenticator.login("Login", "main")
 
 if authenticattion_status == False:
-    st.error('Username/password is incorrect')
+    st.error("Usuário ou senha incorreta")
+    st.session_state["authentication_status"] = False
 
-if authenticattion_status == None:
-    st.warning('Please enter your username and password')
+if authenticattion_status is None:
+    st.warning("Caso não seja cadastrado visite: \nhttp://www.gmail.com")
+    st.session_state["authentication_status"] = None
 
 if authenticattion_status:
-    st.text('Login funcionou - deu tudo certo')
+    st.text("Login funcionou - deu tudo certo")
+    authenticator.logout("Logout", "sidebar")
+    st.session_state["authentication_status"] = True
+    st.session_state["name"] = name
