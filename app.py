74491f969dcc496916b4997a55b903dd693f4f40
@crazyself <crazyself@gmail.com>
2022-07-13 09:33:06 -0300

    Desenvolvimento da primeira versão da página de logina; atualização do README

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from pathlib import Path
+import streamlit as st
+import streamlit_authenticator as stauth
+
+# User authentication
+names = ["Almir Martins", "Leonardo Cordeiro"]
+usernames = ["almir", "leo"]
+
+# Load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(
+    names, usernames, hashed_passwords, 
+    'cookie_named_cu_sujo', 'abcdef', cookie_expiry_days=1)
+
+name, authenticattion_status, username = authenticator.login('Login', 'main')
+
+if authenticattion_status == False:
+    st.error('Username/password is incorrect')
+
+if authenticattion_status == None:
+    st.warning('Please enter your username and password')
+
+if authenticattion_status:
+    st.text('Login funcionou - deu tudo certo')
